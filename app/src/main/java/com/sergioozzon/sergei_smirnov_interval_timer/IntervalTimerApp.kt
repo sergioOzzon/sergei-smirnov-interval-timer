56bc051b664a5c93be39802192f76765f9deab8a
Sergei Smirnov <sergei.smirnov@cleverpumpkin.ru>
2026-04-28 10:22:35 +0300

    add main module

---
+++ app/src/main/java/com/sergioozzon/sergei_smirnov_interval_timer/IntervalTimerApp.kt
@@ -0,0 +1,30 @@
+package com.sergioozzon.sergei_smirnov_interval_timer
+
+import android.app.Application
+import com.learning.intervaltimer.di.data.dataModule
+import com.learning.intervaltimer.di.mainModule
+import com.learning.intervaltimer.di.network.networkModule
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class IntervalTimerApp : Application() {
+
+    override fun onCreate() {
+        super.onCreate()
+        initKoin()
+    }
+
+    private fun initKoin() {
+        startKoin {
+            androidLogger()
+            androidContext(applicationContext)
+
+            modules(
+                mainModule,
+                //dataModule,
+                //networkModule
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/sergioozzon/sergei_smirnov_interval_timer/base/di/MainModule.kt
@@ -0,0 +1,13 @@
+package com.learning.intervaltimer.di
+
+import com.learning.intervaltimer.ui.screens.SearchWorkoutViewModel
+import com.learning.intervaltimer.ui.screens.WorkoutSharedViewModel
+import com.learning.intervaltimer.ui.screens.WorkoutViewModel
+import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.module
+
+val mainModule = module {
+
+
+
+}
