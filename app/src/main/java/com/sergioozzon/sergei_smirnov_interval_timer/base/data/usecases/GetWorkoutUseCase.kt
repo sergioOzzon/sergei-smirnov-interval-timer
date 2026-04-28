0ae4ac35882d477ab9416320dd5e4b66b3b5399c
Sergei Smirnov <sergei.smirnov@cleverpumpkin.ru>
2026-04-28 10:47:30 +0300

    add GetWorkoutUseCase

---
+++ app/src/main/java/com/sergioozzon/sergei_smirnov_interval_timer/base/data/usecases/GetWorkoutUseCase.kt
@@ -0,0 +1,27 @@
+package com.sergioozzon.sergei_smirnov_interval_timer.base.data.usecases
+
+import com.sergioozzon.sergei_smirnov_interval_timer.base.network.mappers.toDTO
+import com.sergioozzon.sergei_smirnov_interval_timer.base.network.services.WorkoutService
+import com.sergioozzon.sergei_smirnov_interval_timer.domain.WorkoutDTO
+
+interface GetWorkoutUseCase {
+
+    suspend operator fun invoke(
+        workoutId: String,
+    ): Result<WorkoutDTO>
+
+}
+
+internal class GetWorkoutUseCaseImpl(
+    private val workoutService: WorkoutService,
+) : GetWorkoutUseCase {
+
+    override suspend fun invoke(workoutId: String): Result<WorkoutDTO> {
+        return runCatching {
+            workoutService
+                .getWorkout(workoutId)
+                .toDTO()
+        }
+    }
+
+}
--- app/src/main/java/com/sergioozzon/sergei_smirnov_interval_timer/base/di/data/DataModule.kt
+++ app/src/main/java/com/sergioozzon/sergei_smirnov_interval_timer/base/di/data/DataModule.kt
@@ -1,10 +1,13 @@
 package com.sergioozzon.sergei_smirnov_interval_timer.base.di.data
 
+import com.sergioozzon.sergei_smirnov_interval_timer.base.data.usecases.GetWorkoutUseCase
+import com.sergioozzon.sergei_smirnov_interval_timer.base.data.usecases.GetWorkoutUseCaseImpl
 import org.koin.core.module.dsl.factoryOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 
 val dataModule = module {
     // UseCases
+    factoryOf(::GetWorkoutUseCaseImpl) bind GetWorkoutUseCase::class
 
 }
